399346b27242d61cce8e9aeee82655851fef9748
Putu Aditya <adityatranasuta@gmail.com>
2021-07-01 18:55:13 +0700

    Add firebase performance Change OTA updates

--- android/app/src/main/java/com/portalnesia/app/generated/BasePackageList.java
+++ android/app/src/main/java/com/portalnesia/app/generated/BasePackageList.java
@@ -15,20 +15,21 @@ public class BasePackageList {
         new expo.modules.crypto.CryptoPackage(),
         new expo.modules.device.DevicePackage(),
         new expo.modules.documentpicker.DocumentPickerPackage(),
+        new expo.modules.errorrecovery.ErrorRecoveryPackage(),
         new expo.modules.filesystem.FileSystemPackage(),
         new expo.modules.font.FontLoaderPackage(),
         new expo.modules.google.signin.GoogleSignInPackage(),
+        new expo.modules.imageloader.ImageLoaderPackage(),
+        new expo.modules.permissions.PermissionsPackage(),
         new expo.modules.imagepicker.ImagePickerPackage(),
         new expo.modules.intentlauncher.IntentLauncherPackage(),
         new expo.modules.location.LocationPackage(),
         new expo.modules.medialibrary.MediaLibraryPackage(),
         new expo.modules.notifications.NotificationsPackage(),
-        new expo.modules.permissions.PermissionsPackage(),
         new expo.modules.securestore.SecureStorePackage(),
         new expo.modules.splashscreen.SplashScreenPackage(),
         new expo.modules.updates.UpdatesPackage(),
-        new expo.modules.webbrowser.WebBrowserPackage(),
-        new expo.modules.imageloader.ImageLoaderPackage()
+        new expo.modules.webbrowser.WebBrowserPackage()
     );
   }
 }
--- utils/axios.ts
+++ utils/axios.ts
@@ -1,7 +1,8 @@
 import {API as APII} from '@env'
-import axios from 'axios'
+import axios, { AxiosRequestConfig, AxiosResponse } from 'axios'
 import * as Application from 'expo-application'
 import {Constants} from 'react-native-unimodules'
+import perf,{FirebasePerformanceTypes} from '@react-native-firebase/perf'
 
 const API = axios.create({
     baseURL:APII,
@@ -12,5 +13,53 @@ const API = axios.create({
         'X-Session-Id':Application.androidId
     }
 })
+
+interface CustomRequestConfig extends AxiosRequestConfig {
+    metadata?: {
+        httpMetric: FirebasePerformanceTypes.HttpMetric
+    }
+}
+
+async function requestAxios(config: AxiosRequestConfig){
+    const new_config:CustomRequestConfig = config;
+    try {
+        const method = (new_config.method as FirebasePerformanceTypes.HttpMethod)
+        const httpMetric = perf().newHttpMetric(new_config.url,method);
+        new_config.metadata = { httpMetric }
+
+        await httpMetric.start();
+    } finally {
+        return new_config;
+    }
+}
+
+interface CustomResponse extends AxiosResponse<any> {
+    config: CustomRequestConfig
+}
+
+async function responseAxios(response: AxiosResponse<any>) {
+    const new_response:CustomResponse = response;
+    try {
+        const {httpMetric} = new_response.config.metadata;
+        httpMetric.setHttpResponseCode(new_response.status);
+        httpMetric.setResponseContentType(new_response.headers['content-type']);
+        await httpMetric.stop();
+    } finally {
+        return new_response;
+    }
+}
+async function errorAxios(e:any){
+    try {
+        const {httpMetric} = e?.config?.metadata;
+        httpMetric.setHttpResponseCode(e?.reponse?.status);
+        httpMetric.setResponseContentType(e?.reponse?.headers['content-type']);
+        await httpMetric.stop();
+    } finally {
+        return Promise.reject(e);
+    }
+}
+
+API.interceptors.request.use(requestAxios)
+API.interceptors.response.use(responseAxios,errorAxios);
 
 export default API;
