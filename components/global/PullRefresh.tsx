501581f08f6b14ee456c0568f6c76ea2a97b1f50
Putu Aditya <adityatranasuta@gmail.com>
2021-07-12 00:44:27 +0700

    Bug momentum scroll

--- components/global/PullRefresh.tsx
+++ components/global/PullRefresh.tsx
@@ -1,13 +1,30 @@
-import React from 'react'
-import { View, ViewStyle } from 'react-native'
-import {Layout,Text} from '@ui-kitten/components'
+import React, { PureComponent } from 'react'
+import { View, ViewStyle,Animated,PanResponder, PanResponderInstance, GestureResponderEvent, PanResponderGestureState,NativeSyntheticEvent, NativeScrollEvent } from 'react-native'
+import {Layout} from '@ui-kitten/components'
 import { AuthContext } from '@pn/provider/Context';
 import LottieView from 'lottie-react-native';
-import PullToRefresh,{PullToRefreshHeaderProps,Props as PullToRefreshProps} from 'react-native-pull-to-refresh-custom'
 
-export interface PullRefreshProps {
+interface PullToRefreshHeaderProps {
+    pullDistance: number;
+    percentAnimatedValue: Animated.AnimatedDivision;
+    percent: number;
     refreshing: boolean;
-    onRefresh(): void;
+}
+
+export interface PullToRefreshProps {
+    style?: ViewStyle;
+    headerHeight: number;
+    refreshingHoldHeight?: number;
+    refreshing: boolean;
+    onRefresh: () => void;
+    children: JSX.Element;
+    topPullThreshold?: number;
+    scrollRef?: any;
+}
+
+interface State {
+    containerTop: Animated.Value;
+    scrollEnabled: boolean;
 }
 
 type HeaderState = {
@@ -37,10 +54,11 @@ class HeaderClass extends React.PureComponent<HeaderClassProps,HeaderState> {
         if(prevProps.percent !== this.props.percent) {
             this.setState({percent:this.props.percent})
         }
-        if(prevProps.refreshing !== this.props.refreshing) {
-            if(this.props.refreshing) {
-                this.lottie.current?.play();
-            }
+        if(!prevProps.refreshing && this.props.refreshing) {
+            this.lottie.current?.play();
+        }
+        if(prevProps.refreshing && !this.props.refreshing) {
+            this.lottie.current?.reset();
         }
     }
 
@@ -53,19 +71,17 @@ class HeaderClass extends React.PureComponent<HeaderClassProps,HeaderState> {
 
     render() {
         const {theme} = this.props;
-        const {percent,pullDistance} = this.state
-
-        //const progress = (0.4*percent)/1
-        //console.log(progress)
-
+        const {percent} = this.state
+        const progress = (percent*0.35)
         return (
-            <Layout level="2" style={{justifyContent:'center',alignItems: 'center'}}>
+            <View style={{justifyContent:'center',alignItems: 'center'}}>
                 <LottieView
                     ref={this.lottie}
-                    style={{height:100}}
+                    style={{height:40,marginTop:10,marginBottom:30}}
+                    progress={progress}
                     source={theme==='dark' ? require('@pn/assets/animation/spinner-dark.json') : require('@pn/assets/animation/spinner-light.json')}
                 />
-            </Layout>
+            </View>
         )
     }
 }
@@ -77,16 +93,216 @@ const Header=React.forwardRef<HeaderClass,PullToRefreshHeaderProps>((props,ref)=
     return <HeaderClass ref={ref} {...props} theme={theme} />
 })
 
-function PullRefreshComp({...props}: PullToRefreshProps) {
-    return (
-        <PullToRefresh
-            HeaderComponent={Header}
-            headerHeight={100}
-            style={{flex:1}}
-            {...props}
-        />
-    )
-}
+const refreshTriggerHeight = 100;
+export default class PullRefresh extends PureComponent<PullToRefreshProps,State> {
+    static defaultProps = {
+        refreshing: false,
+        topPullThreshold: 2,
+    };
+    containerTranslateY: number = 0;
+    innerScrollTop: number = 0;
+    _panResponder: PanResponderInstance;
+    headerRef: any = null;
+    scrollRef: any = null;
 
-const PullRefresh = React.memo(PullRefreshComp);
-export default PullRefresh;
+    constructor(props: PullToRefreshProps) {
+        super(props);
+
+        this.state = {
+            containerTop: new Animated.Value(0),
+            scrollEnabled: false,
+        };
+
+        this.state.containerTop.addListener(this.containerTopChange);
+
+        this.onMoveShouldSetResponder = this.onMoveShouldSetResponder.bind(this);
+        this.onResponderGrant = this.onResponderGrant.bind(this);
+        this.onResponderReject = this.onResponderReject.bind(this);
+        this.onPanResponderMove = this.onPanResponderMove.bind(this);
+        this.onPanResponderRelease = this.onPanResponderRelease.bind(this);
+        this.onPanResponderTerminate = this.onPanResponderTerminate.bind(this);
+        this.onResponderTerminationRequest = this.onResponderTerminationRequest.bind(this);
+        this.onScroll = this.onScroll.bind(this);
+
+        this._panResponder = PanResponder.create({
+            onMoveShouldSetPanResponderCapture: this.onMoveShouldSetResponder,
+            onPanResponderGrant: this.onResponderGrant,
+            onPanResponderReject: this.onResponderReject,
+            onPanResponderMove: this.onPanResponderMove,
+            onPanResponderRelease: this.onPanResponderRelease,
+            onPanResponderTerminationRequest: this.onResponderTerminationRequest,
+            onPanResponderTerminate: this.onPanResponderTerminate,
+            onShouldBlockNativeResponder: (evt, gestureState) => {
+                // Returns whether this component should block native components from becoming the JS
+                // responder. Returns true by default. Is currently only supported on android.
+                return true;
+              },
+        });
+    }
+
+    updateInnerScrollRef = (ref: any) => {
+        this.scrollRef = ref;
+        if(this.props.scrollRef) this.props.scrollRef(ref)
+    };
+
+    onMoveShouldSetResponder(event: GestureResponderEvent, gestureState: PanResponderGestureState) {
+        if (this.props.refreshing) {
+            return false;
+        }
+        return !this.state.scrollEnabled;
+    }
+
+    onResponderGrant(event: GestureResponderEvent, gestureState: PanResponderGestureState) {
+    }
+
+    onResponderReject(event: GestureResponderEvent, gestureState: PanResponderGestureState) {
+    }
+
+    onPanResponderMove(event: GestureResponderEvent, gestureState: PanResponderGestureState) {
+        if (gestureState.dy >= 0) {
+            if(gestureState.dy < (refreshTriggerHeight + 10)) this.state.containerTop.setValue(gestureState.dy);
+        } else {
+            this.state.containerTop.setValue(0);
+            if (this.scrollRef) {
+                if (typeof this.scrollRef.scrollToOffset === 'function') {
+                    this.scrollRef.scrollToOffset({
+                        offset: -gestureState.dy,
+                        animated: true,
+                    });
+                } else if(typeof this.scrollRef.scrollTo === 'function') {
+                    this.scrollRef.scrollTo({
+                        y: -gestureState.dy,
+                        animated: true,
+                    });
+                }
+            }
+        }
+    }
+
+    onPanResponderRelease(event: GestureResponderEvent, gestureState: PanResponderGestureState) {
+        const threshold = refreshTriggerHeight;
+        if (this.containerTranslateY >= threshold) {
+            this.props.onRefresh();
+        } else {
+            this._resetContainerPosition();
+        }
+        this.checkScroll();
+    }
+
+    onResponderTerminationRequest(event: GestureResponderEvent): boolean {
+        // console.log(`====== terminate request`);
+        return false;
+    }
+
+    onPanResponderTerminate(event: GestureResponderEvent, gestureState: PanResponderGestureState) {
+        this._resetContainerPosition();
+        this.checkScroll();
+    }
+
+    _resetContainerPosition() {
+        Animated.timing(this.state.containerTop, {
+            toValue: 0,
+            duration: 250,
+            useNativeDriver: true,
+        }).start();
+    }
+
+    containerTopChange = ({ value }: { value: number }) => {
+        this.containerTranslateY = value;
+        if (this.headerRef) {
+            this.headerRef.setProgress({
+                pullDistance: value,
+                percent: value / (refreshTriggerHeight),
+            });
+        }
+    };
+
+    onScroll = (event: NativeSyntheticEvent<NativeScrollEvent>) => {
+        this.innerScrollTop = event.nativeEvent.contentOffset.y;
+        this.checkScroll();
+    };
+
+    checkScroll = () => {
+        if (this.isInnerScrollTop()) {
+            if (this.state.scrollEnabled) {
+                this.setState({
+                    scrollEnabled: false,
+                });
+            }
+        } else {
+            if (!this.state.scrollEnabled) {
+                this.setState({
+                    scrollEnabled: true,
+                });
+            }
+        }
+    };
+
+    isInnerScrollTop() {
+        return this.innerScrollTop <= this.props.topPullThreshold;
+    }
+
+    componentDidUpdate(prevProps: Readonly<PullToRefreshProps>, prevState: Readonly<State>) {
+        if (!prevProps.refreshing && this.props.refreshing) {
+            const holdHeight = this.props.refreshingHoldHeight || refreshTriggerHeight;
+            Animated.timing(this.state.containerTop, {
+                toValue: holdHeight,
+                duration: 150,
+                useNativeDriver: true,
+            }).start();
+        } else if (prevProps.refreshing && !this.props.refreshing) {
+            Animated.timing(this.state.containerTop, {
+                toValue: 0,
+                duration: 250,
+                useNativeDriver: true,
+            }).start();
+        }
+    }
+
+    componentWillUnmount() {
+        this.state.containerTop.removeAllListeners();
+    }
+
+    renderHeader() {
+        const style = {
+            left: 0,
+            width: '100%',
+            top: this.props.headerHeight,
+            //transform: [{ translateY: this.state.containerTop }],
+        };
+        const percent = Animated.divide(this.state.containerTop, refreshTriggerHeight);
+        
+        return (
+            <Animated.View style={[style,{position:"absolute"}]}>
+                <Header
+                    ref={(c: any) => { this.headerRef = c; }}
+                    percentAnimatedValue={percent}
+                    pullDistance={this.containerTranslateY}
+                    percent={this.containerTranslateY / this.props.headerHeight}
+                    refreshing={this.props.refreshing}
+                />
+            </Animated.View>
+        );
+    }
+
+    render() {
+        const child = React.cloneElement(this.props.children, {
+            bounces: false,
+            alwaysBounceVertical: false,
+            scrollEnabled: this.state.scrollEnabled,
+            ref: this.updateInnerScrollRef,
+        });
+        return (
+            <Layout
+                level="2"
+                style={[this.props.style,{flex:1}]}
+                {...this._panResponder.panHandlers}
+            >
+                {this.renderHeader()}
+                <Animated.View style={[{ flex: 1, transform: [{ translateY: this.state.containerTop }] }]}>
+                    {child}
+                </Animated.View>
+            </Layout>
+        );
+    }
+}
--- screens/News/NewsDetail.js
+++ screens/News/NewsDetail.js
@@ -20,7 +20,7 @@ import Skeleton from '@pn/components/global/Skeleton'
 import Comment from '@pn/components/global/Comment'
 import usePost from '@pn/utils/API'
 import {pushTo} from '@pn/navigation/useRootNavigation'
-//import {CONTENT_URL} from '@env'
+import PullRefresh from '@pn/components/global/PullRefresh'
 
 //const MoreIcon=(props)=><Icon {...props} name="more-vertical" />
 
@@ -32,10 +32,28 @@ export default function({navigation,route}){
     const [open,setOpen]=React.useState(false)
     const [ready,setReady]=React.useState(false)
     const [liked,setLiked] = React.useState(false)
+    const pullRefreshRef = React.useRef(null);
     const heightt = {...headerHeight,sub:0}	
-    const {translateY,...other} = useHeader()
+
+    const onScroll=(e)=>{
+        pullRefreshRef.current?.onScroll(e)
+    }
+
+    const {translateY,...other} = useHeader(58,onScroll)
 	const heightHeader = heightt?.main + heightt?.sub
     const {PNget} = usePost();
+    const [refreshing,setRefreshing] = React.useState(false);
+
+    const onRefresh=React.useCallback(()=>{
+        if(!isValidating) {
+            setRefreshing(true);
+            mutate();
+        }
+    },[isValidating])
+
+    React.useEffect(()=>{
+        if(!isValidating) setRefreshing(false)
+    },[isValidating])
 
     React.useEffect(()=>{
         let timeout = null;
@@ -82,65 +100,64 @@ export default function({navigation,route}){
             <Animated.View style={{position:'absolute',backgroundColor: theme['background-basic-color-1'],left: 0,right: 0,width: '100%',zIndex: 1,transform: [{translateY}]}}>
 				<Header title={"News"} subtitle={data?.title||""} withBack navigation={navigation} height={56} menu={()=><MenuToggle onPress={()=>{data && !data?.error && setOpen(true)}} />} />
 			</Animated.View>
-            <Animated.ScrollView
-                contentContainerStyle={{
-                    flexGrow: 1,
-                    paddingTop:heightHeader+2
-                }}
-                refreshControl={
-                    <RefreshControl colors={['white']} progressBackgroundColor="#2f6f4e" progressViewOffset={heightHeader} refreshing={isValidating && (typeof data !== 'undefined' || typeof error !== 'undefined')} onRefresh={()=>!isValidating && mutate()}/>
-                }
-                {...other}
-            >
-                {!data && !error ? (
-                    <View style={{height:'100%',paddingTop:15}}>
-                        <Skeleton type="article" />
-                    </View>
-                ) : error || data?.error ? (
-                    <NotFound status={data?.code||503}><Text>{data?.msg||"Something went wrong"}</Text></NotFound>
-                ) : data?.text ? (
-                    <>
-                        <Lay style={[style.container]}>
-                            <Text category="h2" style={{paddingVertical:10}}>{data?.title}</Text>
-                            <Lay style={{paddingTop:5,flexDirection:'row',alignItems:'center',justifyContent:'space-between'}}>
-                                <Text numberOfLines={1} style={{flex:1,marginRight:20,fontSize:13}}>{`Last modified ${data?.date_string}`}</Text>
-                                <Text style={{fontSize:13}}><Text><CountUp data={data?.seen} /></Text> <Text>views</Text></Text>
-                            </Lay>
-                        </Lay>
-                        <Lay style={{paddingVertical:20}}><Divider style={{backgroundColor:theme['border-text-color']}} /></Lay>
-                        <Lay style={{paddingBottom:20}}><Parser source={data.text} selectable /></Lay>
-                        <Lay>
-                            <Lay style={{flexDirection:'row',justifyContent:'center',alignItems:'center'}}>
-                                <Button onPress={()=>openBrowser(data?.url)} text>Artikel Asli</Button>
-                            </Lay>
-                        </Lay>
-                        <Lay style={{paddingVertical:20}}><Divider style={{backgroundColor:theme['border-text-color']}} /></Lay>
-                        {data && data?.id ? (
-                            <>
-                                <Lay style={{paddingBottom:20}}>
-                                    <Text category="h5" style={{paddingHorizontal:15,marginBottom:15}}>{i18n.t('recommended')}</Text>
-                                    {(!dataOthers && !errorOthers) || isValidatingOthers ? <Lay style={{paddingHorizontal:15}}><Skeleton type='caraousel' image height={300} /></Lay>
-                                    : errorOthers || dataOthers?.error==1 ? (
-                                        <Text style={{paddingHorizontal:15}}>Failed to load data</Text>
-                                    ) : dataOthers?.data?.length > 0 ? (
-                                        <Carousel
-                                            data={dataOthers?.data}
-                                            renderItem={(props)=><RenderCaraousel {...props} />}
-                                            autoplay
-                                        />
-                                    ) : (
-                                        <Text style={{paddingHorizontal:15}}>No posts</Text>
-                                    )}
+            <PullRefresh ref={pullRefreshRef} onRefresh={onRefresh} refreshing={refreshing} headerHeight={heightHeader+2}>
+                <Animated.ScrollView
+                    contentContainerStyle={{
+                        flexGrow: 1,
+                        paddingTop:heightHeader+2
+                    }}
+                    {...other}
+                >
+                    {!data && !error ? (
+                        <View style={{height:'100%',paddingTop:15}}>
+                            <Skeleton type="article" />
+                        </View>
+                    ) : error || data?.error ? (
+                        <NotFound status={data?.code||503}><Text>{data?.msg||"Something went wrong"}</Text></NotFound>
+                    ) : data?.text ? (
+                        <>
+                            <Lay style={[style.container]}>
+                                <Text category="h2" style={{paddingVertical:10}}>{data?.title}</Text>
+                                <Lay style={{paddingTop:5,flexDirection:'row',alignItems:'center',justifyContent:'space-between'}}>
+                                    <Text numberOfLines={1} style={{flex:1,marginRight:20,fontSize:13}}>{`Last modified ${data?.date_string}`}</Text>
+                                    <Text style={{fontSize:13}}><Text><CountUp data={data?.seen} /></Text> <Text>views</Text></Text>
                                 </Lay>
-                                <Divider style={{backgroundColor:theme['border-text-color']}} />
-                                <Lay style={{paddingBottom:50,paddingTop:10}}>
-                                    <Comment navigation={navigation} total={data?.comment_count} type="news" posId={data?.id} posUrl={`news/${source}/${title}`} />
+                            </Lay>
+                            <Lay style={{paddingVertical:20}}><Divider style={{backgroundColor:theme['border-text-color']}} /></Lay>
+                            <Lay style={{paddingBottom:20}}><Parser source={data.text} selectable /></Lay>
+                            <Lay>
+                                <Lay style={{flexDirection:'row',justifyContent:'center',alignItems:'center'}}>
+                                    <Button onPress={()=>openBrowser(data?.url)} text>Artikel Asli</Button>
                                 </Lay>
-                            </>
-                        ): null}
-                    </>
-                ) : null}
-            </Animated.ScrollView>
+                            </Lay>
+                            <Lay style={{paddingVertical:20}}><Divider style={{backgroundColor:theme['border-text-color']}} /></Lay>
+                            {data && data?.id ? (
+                                <>
+                                    <Lay style={{paddingBottom:20}}>
+                                        <Text category="h5" style={{paddingHorizontal:15,marginBottom:15}}>{i18n.t('recommended')}</Text>
+                                        {(!dataOthers && !errorOthers) || isValidatingOthers ? <Lay style={{paddingHorizontal:15}}><Skeleton type='caraousel' image height={300} /></Lay>
+                                        : errorOthers || dataOthers?.error==1 ? (
+                                            <Text style={{paddingHorizontal:15}}>Failed to load data</Text>
+                                        ) : dataOthers?.data?.length > 0 ? (
+                                            <Carousel
+                                                data={dataOthers?.data}
+                                                renderItem={(props)=><RenderCaraousel {...props} />}
+                                                autoplay
+                                            />
+                                        ) : (
+                                            <Text style={{paddingHorizontal:15}}>No posts</Text>
+                                        )}
+                                    </Lay>
+                                    <Divider style={{backgroundColor:theme['border-text-color']}} />
+                                    <Lay style={{paddingBottom:50,paddingTop:10}}>
+                                        <Comment navigation={navigation} total={data?.comment_count} type="news" posId={data?.id} posUrl={`news/${source}/${title}`} />
+                                    </Lay>
+                                </>
+                            ): null}
+                        </>
+                    ) : null}
+                </Animated.ScrollView>
+            </PullRefresh>
         </Layout>
         {data && !data?.error && (
             <MenuContainer
